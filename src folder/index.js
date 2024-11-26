05cf2035027d13d2f52819cded23926554d458c0
rinitsi <banitsirachel@gmailcom>
2024-11-26 06:53:38 +0000

    added new files

---
+++ src folder/index.js
@@ -0,0 +1,20 @@
+alert(" Hello World");
+//alert("Meet Rachel Banitsi");
+//declaring a variable named Rachel
+//let Rachel=prompt("What is your name?");
+//concatenating variable and strings
+//alert(Rachel + " I know");
+//concatenating strings
+alert("2" +"2");
+
+//variable naming conventions
+//let friend = prompt("What is your favorite freinds name")
+//let age =prompt("What is his or her age")
+//let favColour = prompt("What is his or her favclour")
+//alert(" My beautiful friend's name is " + friend + " She is " + age + " years old." + " Her favcolor is " + favColour)
+
+//backticks
+let friend = prompt("What is your favorite freinds name");
+let age =prompt("What is her age")
+let favColor = prompt("What is her favcolor")
+alert(`My beautiful Friend's name is ${friend}. She is ${age} years old. Her Favcolor is ${favColor}`)
